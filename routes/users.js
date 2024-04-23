65faa2c18910ccca1f9e865b12540dfbda54bef1
Jack Kraus <jackkraus3856@gmail.com>
2024-04-23 15:52:13 -0400

    Added Partial nav and individual application page

--- routes/users.js
+++ routes/users.js
@@ -3,16 +3,15 @@ import { Router } from 'express';
 import data from '../public/data/jobTrackerApplication.users.json' with { type: "json" };
 const router = Router();
 
-router.route('/applications').get(async (_req, res) => {
+router.route('/dashboard').get(async (_req, res) => {
   const applications = data.find((item) => item._id.$oid==="661b1f0f00d5d86b38607305").applications;
-  return res.render("applicationList", {applications:applications});
+  return res.render("dashboard", {applications:applications, nav: 'privateNav'});
 });
 
 router.route('/applications/:id').get(async (req, res) => {
   const applications = data.find((item) => item._id.$oid==="661b1f0f00d5d86b38607305").applications;
-  console.log(applications);
   const application = applications.find((item) => item._id.$oid===req.params.id);
-  return res.json(application);
+  return res.render("applicationPage", {nav: 'privateNav', ...application});
 });
 
 export default router;
