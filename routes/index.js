6506cb0997820aa9578510ce8ae22011b2990dc7
Jack Kraus <jackkraus3856@gmail.com>
2024-04-23 15:25:55 -0400

    Reworked Application page to be Handlebars

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 //Here you will import route files and export the constructor method as shown in lecture code and worked in previous labs.
 import userRoutes from './users.js';
-import apiRoutes from './api.js';
 
 const constructorMethod = (app) => {
   app.use('/', userRoutes);
-  app.use('/api', apiRoutes);
+  app.use('*', (_req, res) => {
+    return res.status(404).json({error:"404 error"});
+  });
 };
-  
+
 export default constructorMethod;
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,18 @@
 //import express, express router as shown in lecture code
 import { Router } from 'express';
+import data from '../public/data/jobTrackerApplication.users.json' with { type: "json" };
 const router = Router();
 
-router.route('/').get(async (_req, res) => {
-  return res.json({error: 'Placeholder'});
+router.route('/applications').get(async (_req, res) => {
+  const applications = data.find((item) => item._id.$oid==="661b1f0f00d5d86b38607305").applications;
+  return res.render("applicationList", {applications:applications});
 });
 
-router.route('/applications').get(async (_req, res) => {
-  return res.render("applicationList", {});
+router.route('/applications/:id').get(async (req, res) => {
+  const applications = data.find((item) => item._id.$oid==="661b1f0f00d5d86b38607305").applications;
+  console.log(applications);
+  const application = applications.find((item) => item._id.$oid===req.params.id);
+  return res.json(application);
 });
 
 export default router;
