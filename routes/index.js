7eacf7e76749f0494f1c696f8d9cb0e49b05f822
Carmen G Vargas <carmengvargas@Carmens-MacBook-Pro.local>
2024-04-28 17:01:22 -0400

    Updated routes

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+//Here you will import route files and export the constructor method as shown in lecture code and worked in previous labs.
+import userRoutes from './users.js';
+import registerRoute from './register.js';
+import loginRoute from './login.js';
+
+const constructorMethod = (app) => {
+  app.use('/', userRoutes);
+  app.use('/register', registerRoute);
+  app.use('/login', loginRoute);
+  app.use('*', (_req, res) => {
+    return res.status(404).json({error:"404 error"});
+  });
+};
+
+export default constructorMethod;
---
+++ routes/users.js
@@ -0,0 +1,17 @@
+//import express, express router as shown in lecture code
+import { Router } from 'express';
+import data from '../public/data/jobTrackerApplication.users.json' with { type: "json" };
+const router = Router();
+
+router.route('/dashboard').get(async (_req, res) => {
+  const applications = data.find((item) => item._id.$oid==="661b1f0f00d5d86b38607305").applications;
+  return res.render("dashboard", {applications:applications, nav: 'privateNav'});
+});
+
+router.route('/applications/:id').get(async (req, res) => {
+  const applications = data.find((item) => item._id.$oid==="661b1f0f00d5d86b38607305").applications;
+  const application = applications.find((item) => item._id.$oid===req.params.id);
+  return res.render("applicationPage", {nav: 'privateNav', ...application});
+});
+
+export default router;
