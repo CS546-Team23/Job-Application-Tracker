38ccc7e6ca5a9c18d5374ff6a1de14df121e4f3d
Jack Kraus <jackkraus3856@gmail.com>
2024-05-01 16:43:47 -0400

    Advanced Modal Functionality
    Made the modal a full form with error checking implemented using Carmen's code and editing for the scenario.

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,6 @@ import { Router } from "express";
 import path from "path";
 import user from '../data/users.js';
 import application from '../data/applications.js';
-import xss from 'xss';
-import { validateId } from "../helpers.js";
-import note from '../data/notes.js';
 
 const router = Router();
 
@@ -13,6 +10,7 @@ router.route("/").get(async (_req, res) => {
   return res.sendFile(path.resolve("static/landing.html"));
 });
 
+// TODO: Error Checking
 router.route("/dashboard").get(async (req, res) => {
   const user_info = await user.getUserById(req.session.user.userId);
   return res.render("dashboard", {
@@ -20,11 +18,13 @@ router.route("/dashboard").get(async (req, res) => {
     nav: "privateNav",
     user: req.session.user
   });
+}).post(async (req, res) => {
+  return res.json(req.body);
 });
 
 router.route("/applications/:id").get(async (req, res) => {
   const app = await application.getJobappByid(req.params.id);
   return res.render("applicationPage", { nav: "privateNav", application:app });
-})
+});
 
 export default router;
